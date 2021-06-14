d094edfa4f59be0a12178a28c2f308ea675c157c
razibc93465 <razibc93465@gmail.com>
2021-06-14 09:28:47 +0600

    new work order11

--- resources/views/admin/work-order/create.blade.php
+++ resources/views/admin/work-order/create.blade.php
@@ -93,7 +93,7 @@
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="mobile">Mobile</label>
-                                            <input type="text" id="mobile" name="mobile" class="form-control"
+                                            <input type="text" id="mobile" name="mobile" class="form-control form-control-sm"
                                                 placeholder="01515664762" />
                                         </div>
                                     </div>
@@ -101,7 +101,7 @@
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="alt_mobile">Alter Mobile</label>
-                                            <input type="text" id="alt_mobile" name="alt_mobile" class="form-control"
+                                            <input type="text" id="alt_mobile" name="alt_mobile" class="form-control form-control-sm"
                                                 placeholder="01516664762" />
                                         </div>
                                     </div>
@@ -109,7 +109,7 @@
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="occupation">Occupation</label>
-                                            <input type="email" id="occupation" name="occupation" class="form-control"
+                                            <input type="email" id="occupation" name="occupation" class="form-control form-control-sm"
                                                 placeholder="Enter occupation" aria-label="john.doe" />
                                         </div>
                                     </div>
@@ -165,14 +165,14 @@
                                         <div class="form-group">
                                             <label class="form-label" for="technical-address">Technical Address</label>
                                             <input type="text" id="technical-address" name="technical_address"
-                                                class="form-control" placeholder="Technical Address" />
+                                                class="form-control form-control-sm" placeholder="Technical Address" />
                                         </div>
                                     </div>
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="billing-address">Billing Address</label>
                                             <input type="text" id="billing-address" name="billing_address"
-                                                class="form-control" placeholder="Billing Address" />
+                                                class="form-control form-control-sm" placeholder="Billing Address" />
                                         </div>
                                     </div>
 
@@ -190,28 +190,28 @@
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="pincode2">File1</label>
-                                            <input type="file" name="gmap_location" id="pincode2" class="form-control"
+                                            <input type="file" name="gmap_location" id="pincode2" class="form-control form-control-sm"
                                                 placeholder="658921" />
                                         </div>
                                     </div>
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="city2">File2</label>
-                                            <input type="file" id="city2" name="connect_type" class="form-control"
+                                            <input type="file" id="city2" name="connect_type" class="form-control form-control-sm"
                                                 placeholder="Birmingham" />
                                         </div>
                                     </div>
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="pincode2">File1</label>
-                                            <input type="file" name="gmap_location" id="pincode2" class="form-control"
+                                            <input type="file" name="gmap_location" id="pincode2" class="form-control form-control-sm"
                                                 placeholder="658921" />
                                         </div>
                                     </div>
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="city2">File2</label>
-                                            <input type="file" id="city2" name="connect_type" class="form-control"
+                                            <input type="file" id="city2" name="connect_type" class="form-control form-control-sm"
                                                 placeholder="Birmingham" />
                                         </div>
                                     </div>
@@ -239,7 +239,7 @@
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="vertical-landmark">SCL Id</label>
-                                            <input type="text" name="scl_id" id="vertical-landmark" class="form-control"
+                                            <input type="text" name="scl_id" id="vertical-landmark" class="form-control form-control-sm"
                                                 placeholder="Borough bridge" />
                                         </div>
                                     </div>
@@ -247,7 +247,7 @@
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="pincode2">Gmap Location</label>
-                                            <input type="text" name="gmap_location" id="pincode2" class="form-control"
+                                            <input type="text" name="gmap_location" id="pincode2" class="form-control form-control-sm"
                                                 placeholder="658921" />
                                         </div>
                                     </div>
@@ -255,7 +255,7 @@
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="city2">Connect Type</label>
-                                            <input type="text" id="city2" name="connect_type" class="form-control"
+                                            <input type="text" id="city2" name="connect_type" class="form-control form-control-sm"
                                                 placeholder="Birmingham" />
                                         </div>
                                     </div>
@@ -268,7 +268,7 @@
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="pincode2">Link Id</label>
-                                            <input type="text" id="pincode2" name="link_id" class="form-control"
+                                            <input type="text" id="pincode2" name="link_id" class="form-control form-control-sm"
                                                 placeholder="658921" />
                                         </div>
                                     </div>
@@ -276,7 +276,7 @@
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="city2">Vat</label>
-                                            <input type="text" id="city2" name="vat" class="form-control"
+                                            <input type="text" id="city2" name="vat" class="form-control form-control-sm"
                                                 placeholder="Birmingham" />
                                         </div>
                                     </div>
@@ -296,7 +296,7 @@
                                         <div class="form-group">
                                             <label class="form-label" for="city2">Order Submission Date</label>
                                             <input type="text" id="city2" name="order_submission_date"
-                                                class="form-control" placeholder="Birmingham" />
+                                                class="form-control form-control-sm" placeholder="Birmingham" />
                                         </div>
                                     </div>
 
@@ -307,7 +307,7 @@
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="pincode2">Billing Cycle</label>
-                                            <input type="text" id="pincode2" name="billing_cycle" class="form-control"
+                                            <input type="text" id="pincode2" name="billing_cycle" class="form-control form-control-sm"
                                                 placeholder="658921" />
                                         </div>
                                     </div>
@@ -315,7 +315,7 @@
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="city2">Billing Remark</label>
-                                            <input type="text" id="city2" name="billing_remark" class="form-control"
+                                            <input type="text" id="city2" name="billing_remark" class="form-control form-control-sm"
                                                 placeholder="Birmingham" />
                                         </div>
                                     </div>
@@ -323,7 +323,7 @@
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="pincode2">Bill Start Date</label>
-                                            <input type="text" id="pincode2" name="bill_start_date" class="form-control"
+                                            <input type="text" id="pincode2" name="bill_start_date" class="form-control form-control-sm"
                                                 placeholder="658921" />
                                         </div>
                                     </div>
@@ -331,7 +331,7 @@
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="city2">Delivery Date</label>
-                                            <input type="text" id="city2" name="delivery_date" class="form-control"
+                                            <input type="text" id="city2" name="delivery_date" class="form-control form-control-sm"
                                                 placeholder="Birmingham" />
                                         </div>
                                     </div>
@@ -347,14 +347,14 @@
                                         <div class="form-group">
                                             <label class="form-label" for="pincode2">Bill Generate Method</label>
                                             <input type="text" id="pincode2" name="bill_generate_method"
-                                                class="form-control" placeholder="658921" />
+                                                class="form-control form-control-sm" placeholder="658921" />
                                         </div>
                                     </div>
 
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="city2">Total Price</label>
-                                            <input type="text" id="city2" name="total_Price" class="form-control"
+                                            <input type="text" id="city2" name="total_Price" class="form-control form-control-sm"
                                                 placeholder="Birmingham" />
                                         </div>
                                     </div>
@@ -362,7 +362,7 @@
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="pincode2">Core Rent</label>
-                                            <input type="text" id="pincode2" name="core_rent" class="form-control"
+                                            <input type="text" id="pincode2" name="core_rent" class="form-control form-control-sm"
                                                 placeholder="658921" />
                                         </div>
                                     </div>
@@ -370,7 +370,7 @@
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="city2">OTC</label>
-                                            <input type="text" id="city2" name="otc" class="form-control"
+                                            <input type="text" id="city2" name="otc" class="form-control form-control-sm"
                                                 placeholder="Birmingham" />
                                         </div>
                                     </div>
@@ -384,7 +384,7 @@
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                             <label class="form-label" for="pincode2">Real IP</label>
-                                            <input type="text" id="pincode2" name="real_ip" class="form-control"
+                                            <input type="text" id="pincode2" name="real_ip" class="form-control form-control-sm"
                                                 placeholder="658921" />
                                         </div>
                                     </div>
@@ -393,7 +393,7 @@
                                         <div class="form-group">
                                             <label class="form-label" for="pincode2">Marketing User</label>
                                             <input type="text" id="pincode2" name="marketing_user_id"
-                                                class="form-control" placeholder="658921" />
+                                                class="form-control form-control-sm" placeholder="658921" />
                                         </div>
                                     </div>
 
@@ -401,7 +401,7 @@
                                         <div class="form-group">
                                             <label class="form-label" for="pincode2">customers User</label>
                                             <input type="text" id="pincode2" name="customers_user_id"
-                                                class="form-control" placeholder="658921" />
+                                                class="form-control form-control-sm" placeholder="658921" />
                                         </div>
                                     </div>
 
@@ -409,7 +409,7 @@
                                         <div class="form-group">
                                             <label class="form-label" for="pincode2">Security Money Type</label>
                                             <input type="text" id="pincode2" name="security_money_type"
-                                                class="form-control" placeholder="658921" />
+                                                class="form-control form-control-sm" placeholder="658921" />
                                         </div>
                                     </div>
 
@@ -423,7 +423,7 @@
                                     <div class="col-md-3 col-12">
                                         <div class="form-group">
                                               <label class="form-label" for="pincode2">Security Money Amount</label>
-                                <input type="text" id="pincode2" name="security_money_amount" class="form-control"
+                                <input type="text" id="pincode2" name="security_money_amount" class="form-control form-control-sm"
                                     placeholder="658921" />
                                         </div>
                                     </div>
