cd274aa25d3a3f29663e859448fbb7566d0075a5
razibc93465 <razibc93465@gmail.com>
2021-06-20 16:05:30 +0600

    Report Search ok

--- resources/views/admin/report/pendingResult.blade.php
+++ resources/views/admin/report/pendingResult.blade.php
@@ -23,52 +23,50 @@
                 <th>Record File</th>
             </tr>
             @foreach ($r as $item)
-                <tr>
-                    <td>{{ $item->id }}</td>
-                    <td>
-                        @if ($item->ctype == 'new')
-                            <a href="#" class="btn btn-primary btn-circle col-sm">Pending
-                                <i class="fas fa-check"></i>
-                            </a>
-                        {{-- @elseif($item->ctype == 'followup')
-                            <a href="#" class="btn btn-success btn-circle col-sm">Approved
-                                <i class="fas fa-check"></i>
-                            </a>
-                        @elseif($item->ctype == 'reconnect')
-                            <a href="#" class="btn btn-success btn-circle col-sm">Approved
-                                <i class="fas fa-check"></i>
-                            </a> --}}
-                        @endif
-                    </td>
-                    <td>{{ $item->cname }}</td>
-                    <td>{{ $item->ctype }} </td>
-                    <td>{{ $item->isp_type }} </td>
-                    <td>{{ $item->address }} </td>
-                    <td>{{ $item->district }} </td>
-                    <td>{{ $item->upazila }} </td>
-                    <td>{{ $item->contact_number }}</td>
-                    <td>{{ $item->contact_person }}</td>
-                    <td>{{ $item->email }}</td>
-                    <td>{{ $item->bandwidth }}</td>
-                    <td>{{ $item->rate }}</td>
-                    <td>{{ $item->otc }}</td>
-                    <td>{{ $item->remark }}</td>
-                    <td>{{ $item->visit_phone }}</td>
-                    <td>
-                        <img class="img-fluid" style="width:100px; height: auto;"
-                            src="{{ asset('storage/visitingCard/' . $item->visiting_card) }}" alt="No Image">
-                    </td>
-                    <td>{{ $item->created_at }}</td>
-                    <td>
-                        <div class="audiofile">
-                            <audio controls="">
-                                <source src="{{ asset('storage/audio/' . $item->audio) }}" type="audio/mpeg">
-                                Your browser does not support the audio element.
-                            </audio>
-                        </div>
-                    </td>
+            <tr>
+                <td>{{ $item->id }}</td>
+                <td>
+                    @if ($item->ctype == 'new')
+                    @can('report-approve')
+                    <a href="{{ route('approve', ['id' => $item->id]) }}"
+                        class="btn btn-success btn-circle col-sm">Approve
+                        <i class="fas fa-check"></i>
+                    </a>
+                    @endcan
+                    <a href="#" class="mt-1 btn btn-primary btn-circle col-sm">Pending
+                        <i class="fas fa-check"></i>
+                    </a>
+                    @endif
+                </td>
+                <td>{{ $item->cname }}</td>
+                <td>{{ $item->ctype }} </td>
+                <td>{{ $item->isp_type }} </td>
+                <td>{{ $item->address }} </td>
+                <td>{{ $item->district }} </td>
+                <td>{{ $item->upazila }} </td>
+                <td>{{ $item->contact_number }}</td>
+                <td>{{ $item->contact_person }}</td>
+                <td>{{ $item->email }}</td>
+                <td>{{ $item->bandwidth }}</td>
+                <td>{{ $item->rate }}</td>
+                <td>{{ $item->otc }}</td>
+                <td>{{ $item->remark }}</td>
+                <td>{{ $item->visit_phone }}</td>
+                <td>
+                    <img class="img-fluid" style="width:100px; height: auto;"
+                        src="{{ asset('storage/visitingCard/' . $item->visiting_card) }}" alt="No Image">
+                </td>
+                <td>{{ $item->created_at }}</td>
+                <td>
+                    <div class="audiofile">
+                        <audio controls="">
+                            <source src="{{ asset('storage/audio/' . $item->audio) }}" type="audio/mpeg">
+                            Your browser does not support the audio element.
+                        </audio>
+                    </div>
+                </td>
 
-                </tr>
+            </tr>
             @endforeach
         </tbody>
     </table>
