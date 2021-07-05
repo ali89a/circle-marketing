146a83c60eed358d36e12f9019f037bbcf89d87a
Mohammad Ali <mdali89a@gmail.com>
2021-07-05 12:28:00 +0600

    new invoice bill start date theke start doone

--- resources/views/customer/partials/sidebar.blade.php
+++ resources/views/customer/partials/sidebar.blade.php
@@ -1,5 +1,5 @@
 <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-  <li class="{{ Request::segment(2) == 'home' ? 'active' : '' }} nav-item"><a class="d-flex align-items-center" href="{{ url('/admin/home') }}"><i data-feather="home"></i><span class="menu-title text-truncate" data-i18n="Home">Home</span></a>
+  <li class="{{ Request::segment(2) == 'home' ? 'active' : '' }} nav-item"><a class="d-flex align-items-center" href="{{ url('/home') }}"><i data-feather="home"></i><span class="menu-title text-truncate" data-i18n="Home">Home</span></a>
   </li>
 
   <li class="{{ Request::segment(2) == 'order' ? 'has-sub sidebar-group-active open' : '' }} nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="layout">
